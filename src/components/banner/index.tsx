af7bb14f52432cbd93de149874b5015cbab3c075
Ali Mousazada <mousazada.ali@gmail.com>
2025-03-22 16:17:56 -0400

    create some component and banner

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  Card,
+  CardContent,
+} from '@/components/ui';
+
+import IMG1 from './image/1.webp'
+import IMG2 from './image/2.png'
+import IMG3 from './image/3.jpg'
+import IMG4 from './image/4.webp'
+import Image from 'next/image';
+
+const arrImage = [IMG1 , IMG2 , IMG3 , IMG4]
+
+ function Banner() {
+  return (
+    <Carousel className="w-full">
+      <CarouselContent>
+        {arrImage.map((image, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="relative aspect-square h-[400px] w-full flex items-center justify-center p-6">
+                  <Image src={image} alt='banner' fill className='object-cover' />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
+
+export default Banner
